48c597615fd90b27490f2b352e9ab10915103413
sifanragn <sifanuragnia@gmail.com>
2025-10-02 16:00:22 +0700

    nyoba

--- database/migrations/2025_10_02_041819_create_todos_table.php
+++ database/migrations/2025_10_02_041819_create_todos_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description');
+            $table->boolean('is_done')->default(false);
             $table->timestamps();
         });
     }
